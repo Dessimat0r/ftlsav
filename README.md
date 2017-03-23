5b523b51abde01bf0066eb9c79f852c915da0de6
Chris Dennett <dessimat0r@gmail.com>
2017-03-23 17:49:50 +0000

    Fixed continue.sav stuff

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FTL Savescumming Tool 0.8
+# FTL Savescumming Tool 0.85
 Use this to backup your FTL save when you exit Faster Than Light, and to restore saves when you need them. This won't fix your savescumming conscience though.
 
 ## Installation/Usage
@@ -8,6 +8,9 @@ Make sure it is chmodded as executable, then just run it in the terminal (./ftls
 * Windows/Cygwin (untested, probably doesn't work)
 * OS X (tested, works for sure)
 * Linux (untested, probably works)
+
+## Changelog
+* 0.85: Messed up, missed `continue.sav` which is where the actual continue data is stored. Now integrated.
 
 ![Screenshot 1](/screenshot1.png?raw=true "Screenshot 1")
 ![Screenshot 1](/screenshot2.png?raw=true "Screenshot 2")
--- ftlsav.sh
+++ ftlsav.sh
@@ -13,110 +13,181 @@ else
 	echo "FTL_DIR is set to '$FTL_DIR'";
 	cd $FTL_DIR;
 fi
+IFS=$'\n' # set field separator to newline
 while true; do
-	aesav="(no ae_prof.sav exists)"
+	contsav="does not exist"
+	aesav="does not exist"
 	if [ -e "ae_prof.sav" ];
 	then
 		stat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" ae_prof.sav)
-		aesav=" (ae_prof.sav exists (modified $stat))"
+		aesav="$stat"
 	fi
-	aesavstat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" $filename)
-	menuitem=$(dialog --title "FTL Save Backup" --keep-tite --no-cancel --no-ok --menu "Backup an FTL saved game to another file$aesav." 14 55 5 "Backup" "Backup saved game" "Restore" "Restore saved game" "Rename" "Rename saved game (TODO)" "Delete" "Delete saved game (TODO)" "Quit" "Quit" 2>&1 >/dev/tty)
+	if [ -e "continue.sav" ];
+	then
+		stat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" continue.sav)
+		contsav="$stat"
+	fi
+	menuitem=$(dialog --title "FTL Save Backup" --keep-tite --no-cancel --no-ok --menu "Backup an FTL saved game to another file. Note that you may have a non-AE saved game. This is for AE saves only.\n(*) ae_prof.sav: $aesav\n(*) continue.sav: $contsav" 16 55 5 "Backup" "Backup saved game" "Restore" "Restore saved game" "Rename" "Rename saved game (TODO)" "Delete" "Delete saved game (TODO)" "Quit" "Quit" 2>&1 >/dev/tty)
 	# Return status of non-zero indicates cancel
 	if [ $? -eq 0 ]
 	then
 		case $menuitem in
 		Backup)
-			if [ -e "ae_prof.sav" ];
+			if [ -e "continue.sav" ];
 			then
 				while true; do
 					filename=$(dialog --title "Enter filename" --keep-tite --inputbox "Enter the filename for the saved game" 10 45 2>&1 >/dev/tty)
-					if [ $? -eq 0 ];
+				    res=$?
+					filename="${filename#"${filename%%[![:space:]]*}"}"
+				    filename="${filename%"${filename##*[![:space:]]}"}"
+					if [ $res -eq 0 ];
 					then
-						filename=${filename%".ae.sav.bak"}".ae.sav.bak"
-						if [ $filename != ".ae.sav.bak" ];
+						if [ -z ${filename} ];
 						then
-							echo $filename
+							dialog --msgbox "Must enter a filename." 5 60
+						else
 							save=true;
-							if [ -e $filename ];
+							aeproffn=${filename%".prof.ae.sav.bak"}".prof.ae.sav.bak"
+							echo $aeproffn
+							if [ -e $aeproffn ];
 							then
-								stat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" $filename)
-								dialog --yesno "A file named '$filename' already exists (modified $stat). Overwrite?" 6 60
+								stat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "$aeproffn")
+								dialog --yesno "A file named '$aeproffn' already exists (modified $stat). Overwrite?" 6 60
 								if [ $? -ne 0 ]; then # no
 									save=false
 								fi
 							fi
 							if [ "$save" = true ];
 							then
-								cp -f "ae_prof.sav" "$filename"
-								dialog --msgbox "File 'ae_prof.sav' copied to file '$filename'." 5 60
+								aecontfn=${filename%".cont.ae.sav.bak"}".cont.ae.sav.bak"
+								echo $aecontfn
+								if [ -e $aecontfn ];
+								then
+									stat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "$aecontfn")
+									dialog --yesno "A file named '$aecontfn' already exists (modified $stat). Overwrite?" 6 60
+									if [ $? -ne 0 ]; then # no
+										save=false
+									fi
+								fi
+							fi
+							if [ "$save" = true ];
+							then
+								cp -f "ae_prof.sav" "$aeproffn"
+								cp -f "continue.sav" "$aecontfn"
+								dialog --msgbox "File 'ae_prof.sav' copied to file '$aeproffn'. File 'continue.sav' copied to file '$aecontfn'." 6 60
 								break
 							fi
-						else
-							dialog --msgbox "Must enter a filename." 5 60
 						fi
 					else
 						break
 					fi
 				done
 			else
-				dialog --msgbox "ae_prof.sav doesn't exist. Make sure to save your FTL game first." 5 60
+				dialog --msgbox "continue.sav doesn't exist. Make sure to save your FTL game first." 5 60
 			fi
 			;;
 		Restore)
-			let i=0 # define counting variable
-			w=() # define working array
-			for line in `ls -1 .`
-			do
-				if [[ $line == *.ae.sav.bak ]]; then
-					let i=$i+1
-					cmp=""
-					if [ -e "ae_prof.sav" ];
-					then
-						cmp --silent "$line" "ae_prof.sav"
-						if [ $? -eq 0 ]; then
-							cmp="*"
-						fi
-					fi
-					stat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" $line)
-					w+=("$line" ${line%".ae.sav.bak"}$cmp" ($stat)" )
-				fi
-			done
-			if [ ${#w[@]} -eq 0 ]
-			then # no items
-				dialog --msgbox "No save backups found (they must end in .ae.sav.bak)." 5 60
-			else # have items
-				echo ${w[@]}
-				file=$(dialog --title "Select file to restore" --keep-tite --no-tags --menu "Choose a file to restore to ae_prof.sav (copied file will remain). * indicates this file has the same contents as ae_prof.sav." 15 60 10 "${w[@]}" 2>&1 >/dev/tty) # show dialog and store output
-				if [ $? -eq 0 ]; then # continue
-					save=true;
-					if [ -e "ae_prof.sav" ];
-					then
-						cmp --silent "$file" "ae_prof.sav"
-						if [ $? -eq 0 ]; then
-							dialog --msgbox "File '$file' is the same as 'ae_prof.sav' No reason to overwrite." 6 60
-							save=false
-						fi
-					fi
-					if [ "$save" = true ];
-					then
+			while true; do
+				let i=0 # define counting variable
+				w=() # define working array
+				for line in `ls -1 .`
+				do
+					echo $line
+					if [[ $line == *.prof.ae.sav.bak ]]; then
+						filename=${line%".prof.ae.sav.bak"}
+						let i=$i+1
+						cmp=""
 						if [ -e "ae_prof.sav" ];
 						then
-							aestat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "ae_prof.sav")
-							svstat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "$file")
-							dialog --yesno "A file named 'ae_prof.sav' already exists (modified $aestat). Overwrite with file '$file' (modified $svstat)?" 8 60
-							if [ $? -ne 0 ]; then # no
-								save=false
+							cmp --silent "$line" "ae_prof.sav"
+							if [ $? -eq 0 ]; then
+								cmp+="p"
 							fi
+						fi
+						if [ -e "continue.sav" ];
+						then
+							cmp --silent "$filename.cont.ae.sav.bak" "continue.sav"
+							if [ $? -eq 0 ]; then
+								cmp+="c"
+							fi
+						fi
+						if [ -z ${cmp} ]
+						then
+							cmp=""
+						else
+							cmp=" ($cmp)"
 						fi	
+						stat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "$line")
+						w+=("$line" ${line%".prof.ae.sav.bak"}$cmp" ($stat)" )
+					fi
+				done
+				if [ ${#w[@]} -eq 0 ]
+				then # no items
+					dialog --msgbox "No save backups found. Looking for files ending in '.prof.ae.sav.bak'." 6 60
+					break
+				else # have items
+					echo ${w[@]}
+					file=$(dialog --title "Select file to restore" --keep-tite --no-tags --menu "Choose a save to restore to ae_prof.sav and continue.sav (copied files will remain). (pc) indicates this save has the same contents as ae_prof.sav and continue.sav." 15 60 10 "${w[@]}" 2>&1 >/dev/tty) # show dialog and store output
+					if [ $? -eq 0 ]; then # continue
+						save=true;
+						contfn=${file%".prof.ae.sav.bak"}".cont.ae.sav.bak"
+						if [ -ne $contfn ];
+						then
+							dialog --msgbox "File '$contfn' not found. Both '$file' and '$contfn' files are required." 6 60
+							save=false
+						fi
 						if [ "$save" = true ];
 						then
-							cp -f "$file" "ae_prof.sav"
-							dialog --msgbox "File '$file' restored to 'ae_prof.sav'." 6 60
+							if [ -e "ae_prof.sav" ];
+							then
+								cmp --silent "$file" "ae_prof.sav"
+								if [ $? -eq 0 ]; then
+									if [ -e "continue.sav" ];
+									then
+										cmp --silent "$contfn" "continue.sav"
+										if [ $? -eq 0 ];
+										then
+											dialog --msgbox "File '$file' is the same as 'ae_prof.sav' and file '$contfn' is the same as 'continue.sav' (if it exists). No reason to overwrite." 8 60
+											save=false
+										fi
+									fi
+								fi
+							fi
+							if [ "$save" = true ];
+							then
+								if [ -e "ae_prof.sav" ] || [ -e "continue.sav" ];
+								then
+									aestat="N/A"
+									svstat="N/A"
+									contstat="N/A"
+									contsvstat="N/A"
+									if [ -e "ae_prof.sav" ]; then aestat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "ae_prof.sav"); fi
+									if [ -e $file ]; then svstat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "$file"); fi
+									if [ -e "continue.sav" ]; then contstat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "continue.sav"); fi
+									if [ -e $contfn ]; then contsvstat=$(stat -f "%Sm" -t "%Y-%m-%d @ %H:%M" "$contfn"); fi
+									dialog --yesno "Files named 'ae_prof.sav' and/or 'continue.sav' already exist (modified $aestat and $contstat respectively). Overwrite with files '$file' and '$contfn' (modified $svstat and $contsvstat respectively)?" 10 70
+									if [ $? -ne 0 ]; then # no
+										save=false
+									fi
+								fi
+								if [ "$save" = true ];
+								then
+									line="File '$file' restored to 'ae_prof.sav'."
+									cp -f "$file" "ae_prof.sav"
+									if [ -e $contfn ]; then
+										cp -f "$contfn" "continue.sav"
+										line=" File '$contfn' restored to 'continue.sav'."
+									fi
+									dialog --msgbox "$line" 6 60
+									break
+								fi
+							fi
 						fi
+					else
+						break
 					fi
 				fi
-			fi
+			done
 			;;
 		Quit)
 			break
